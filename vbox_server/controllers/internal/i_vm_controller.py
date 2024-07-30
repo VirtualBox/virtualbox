ee0408bf72b776026a17cfc153a3d9d8b85cecfa
Valery Portnyagin <valery.portnyagin@oracle.com>
2024-07-30 15:30:01 +0400

    feat. Added i_console_attachusbdevice() implementation.

--- vbox_server/controllers/internal/i_vm_controller.py
+++ vbox_server/controllers/internal/i_vm_controller.py
@@ -22,13 +22,14 @@ from vbox_server.utils.restapi_objects_functions import *
 from vbox_server.utils.decorators import session_decorator as sessionDecorator
 from vbox_server.utils.decorators import open_exclusive_session as openExclusiveSession
 from vbox_server.utils.decorators import open_session as openSession
+
+
+############################ Implemented or used ############################
 from vbox_server.models.device_type import DeviceType
 from vbox_server.models.error import Error
 from vbox_server.models.progress import Progress
 from vbox_server.models.progress_response import ProgressResponse
 from vbox_server.models.cleanup_mode import CleanupMode
-
-############################ Implemented or used ############################
 from vbox_server.models.machine_getguestproperty_response import MachineGetguestpropertyResponse  # noqa: E501
 from vbox_server.models.machine_querylogfilename_response import MachineQuerylogfilenameResponse  # noqa: E501
 from vbox_server.models.machine_readlog_response import MachineReadlogResponse  # noqa: E501
@@ -72,7 +73,6 @@ from vbox_server.models.machine_temporary_eject_device_request_body import Machi
 from vbox_server.models.platform_x86_set_cpu_property_request_body import PlatformX86SetCPUPropertyRequestBody  # noqa: E501
 from vbox_server.models.platform_x86_set_hw_virt_ex_property_request_body import PlatformX86SetHWVirtExPropertyRequestBody  # noqa: E501
 from vbox_server.models.virtual_box_open_machine_request_body import VirtualBoxOpenMachineRequestBody  
-############################# Not implemented yet #############################
 
 # Set logging level for module
 logging.getLogger().setLevel(logging.INFO)
@@ -279,7 +279,7 @@ def i_machine_deleteconfig(vmid, media=None):
     if oError is None:
         try:
             logging.info ('Nobody locks the machine. Try to get the lock back for ' + oVM.id)
-            oVBoxMediumList = oVM.unregister(ctx['const'].CleanupMode_Full)
+            oVBoxMediumList = oVM.unregister(ctx['const'].CleanupMode_DetachAllReturnHardDisksOnly)
             oProgress = oVM.deleteConfig(oVBoxMediumList)
         except Exception as e:
             logging.info("Can't delete VM '%s': %s" % (oVM.name, str(e)))
@@ -1675,6 +1675,59 @@ def i_machine_cloneto(vmid, oMachineCloneToRequestBody: MachineCloneToRequestBod
     return response, httpCode
 
 
+@sessionDecorator
+def i_console_attachusbdevice(vmid, oConsoleAttachUSBDeviceRequestBody, *var_args_tuple):  # noqa: E501
+    """
+    Call interface method IConsole::attachUSBDevice
+
+    :param vmid: The Id of vm
+    :type vmid: str
+    :param oConsoleAttachUSBDeviceRequestBody: 
+    :type oConsoleAttachUSBDeviceRequestBody: dict | bytes
+
+    :rtype: None
+    """
+
+    vbox_utils_commonChecks()
+
+    oVM = None
+    oError = None
+    httpCode = HTTPStatus.OK
+
+    logging.info('Passed machine Id is ' + vmid)
+
+    oVM = var_args_tuple[0]
+    oSession = var_args_tuple[1]
+    oCurrMachine = oSession.machine
+    oConsole = oSession.console
+
+    o = oConsoleAttachUSBDeviceRequestBody
+    try:
+        logging.info("Try to attach USB device to the machine " + oVM.name + " (UUID " + oVM.id + ")")
+
+        ol_usb_devices = ctx['global'].getArray(oConsole,'USBDevices')
+        for item in ol_usb_devices:
+            oUsb = i_fill_usb_device(item)
+
+        if o.capture_filename is None or o.capture_filename == '':
+            oConsole.attachUSBDevice(o.id, '')
+        else:
+            oConsole.attachUSBDevice(o.id, o.capture_filename)
+
+        oCurrMachine.saveSettings()
+
+    except Exception as e:
+        logging.info("Exception during attaching USB device to the machine " + oVM.name + " (UUID " + oVM.id + ")")
+        httpCode = HTTPStatus.INTERNAL_SERVER_ERROR
+        oError = Error(httpCode, str(e))
+
+    if oError is not None:
+        response = jsonify(oError)
+        return response, httpCode
+
+    return "Successfully attached USB device with id " + str(o.id) + " to the machine " + oVM.name
+
+
 ############################# Not implemented yet #############################
 def i_console_addencryptionpassword(vmid, oConsoleAddEncryptionPasswordRequestBody):  # noqa: E501
     """
@@ -1699,21 +1752,6 @@ def i_console_addencryptionpasswords(vmid, oConsoleAddEncryptionPasswordsRequest
     :type vmid: str
     :param oConsoleAddEncryptionPasswordsRequestBody: 
     :type oConsoleAddEncryptionPasswordsRequestBody: dict | bytes
-
-    :rtype: None
-    """
-
-    return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
-
-
-def i_console_attachusbdevice(vmid, oConsoleAttachUSBDeviceRequestBody):  # noqa: E501
-    """
-    Call interface method IConsole::attachUSBDevice
-
-    :param vmid: The Id of vm
-    :type vmid: str
-    :param oConsoleAttachUSBDeviceRequestBody: 
-    :type oConsoleAttachUSBDeviceRequestBody: dict | bytes
 
     :rtype: None
     """
