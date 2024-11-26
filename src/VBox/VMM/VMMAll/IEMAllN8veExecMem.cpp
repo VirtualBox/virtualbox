ae1acf12dcc85e208e0bde85748297ba4752b69c
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2024-11-26 22:43:27 +0000

    7.1: Backported r165272 r165291 r165302 r165305 r165367 r165463.
    * r165272: VMM/IEM: Eliminated the IEMNATIVE_WITH_SIMD_REG_ALLOCATOR define. Fixed bug in iemNativeEmitMemFetchStoreDataCommon where a SIMD register was masked in calls to iemNativeVarSaveVolatileRegsPreHlpCall and friends. Fixed theoretical loop-forever bugs in iemNativeSimdRegAllocFindFree & iemNativeRegAllocFindFree.  bugref:10720
    * r165291: VMM/IEM: Added iemNativeEmitLoadGprWithGstReg[Ex]T and iemNativeEmitStoreGprToGstReg[Ex]T as better way of explictly loading & storing standard guest registers. bugref:10720
    * r165302: VMM/Makefile.kmk: Enable agressive inlining for VC++ 2019 for VBoxVMM as it speeds up the native recompilation a bit. Updated my private assembly listing hack to include a sample of the native recompiler code. bugref:10720
    * r165305: VMM/IEM: Some IEMNATIVEVAR / iemNativeVarAllocInt optimization tweaks. bugref:10720
    * r165367: VMM/Makefile.kmk: scm fix. bugref:10720
    * r165463: VMM/IEM: linux build fixes. bugref:10720
    
    
    svn:sync-xref-src-repo-rev: r166140

--- src/VBox/VMM/VMMAll/IEMAllN8veExecMem.cpp
+++ src/VBox/VMM/VMMAll/IEMAllN8veExecMem.cpp
@@ -1,4 +1,4 @@
-/* $Id: IEMAllN8veExecMem.cpp 107184 2024-11-26 22:32:44Z knut.osmundsen@oracle.com $ */
+/* $Id: IEMAllN8veExecMem.cpp 107185 2024-11-26 22:43:27Z knut.osmundsen@oracle.com $ */
 /** @file
  * IEM - Native Recompiler, Executable Memory Allocator.
  */
@@ -322,6 +322,7 @@ typedef IEMEXECMEMALLOCATOR *PIEMEXECMEMALLOCATOR;
  */
 typedef struct IEMEXECMEMALLOCHDR
 {
+    RT_GCC_EXTENSION
     union
     {
         struct
