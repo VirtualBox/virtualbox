c84a3af1cdd29df784b9f740bfa0bbe42ddc83bb
Sergey Dubov <sergey.dubov@oracle.com>
2025-01-20 15:53:43 +0000

    7.1: Backported r167056: FE/Qt: bugref:10767: VM settings / Graphical controller editor: Do not notify listeners about index changes at repopulate moments when combo is empty.
    svn:sync-xref-src-repo-rev: r167057

--- src/VBox/Frontends/VirtualBox/src/settings/editors/UIGraphicsControllerEditor.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/editors/UIGraphicsControllerEditor.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIGraphicsControllerEditor.cpp 106320 2024-10-15 12:08:41Z klaus.espenlaub@oracle.com $ */
+/* $Id: UIGraphicsControllerEditor.cpp 107960 2025-01-20 15:53:43Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIGraphicsControllerEditor class implementation.
  */
@@ -99,7 +99,7 @@ void UIGraphicsControllerEditor::handleFilterChange()
 
 void UIGraphicsControllerEditor::sltHandleCurrentIndexChanged()
 {
-    if (m_pCombo)
+    if (m_pCombo && m_pCombo->count())
         emit sigValueChanged(m_pCombo->itemData(m_pCombo->currentIndex()).value<KGraphicsControllerType>());
 }
 
