dbb270c703298773e80bb560a92777f6af5b13a5
Valery Portnyagin <valery.portnyagin@oracle.com>
2025-08-14 15:48:34 +0400

    fix. Commented out some functions that are generated now. Fixed names according new naming convention.

--- vbox_server/controllers/internal/i_cloudnetwork_controller.py
+++ vbox_server/controllers/internal/i_cloudnetwork_controller.py
@@ -14,47 +14,47 @@ import logging
 from http import HTTPStatus
 from flask import jsonify
 
-from vbox_server.models.cloud_network_response import CloudNetworkResponse  # noqa: E501
+from vbox_server.models.cloud_network_obj_wrapper_response import CloudNetworkObjWrapperResponse  # noqa: E501
 from vbox_server.models.error import Error  # noqa: E501
 from vbox_server import util
 
 
-def i_virtualbox_createcloudnetwork(networkName=None):  # noqa: E501
-    """
-    Call interface method IVirtualBox::createCloudNetwork
+# def i_virtualbox_createcloudnetwork(networkName=None):  # noqa: E501
+#     """
+#     Call interface method IVirtualBox::createCloudNetwork
 
-    :param networkName: 
-    :type networkName: str
+#     :param networkName: 
+#     :type networkName: str
 
-    :rtype: CloudNetworkResponse
-    """
+#     :rtype: CloudNetworkResponse
+#     """
 
-    return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
-
-
-def i_virtualbox_findcloudnetworkbyname(select=None, networkName=None):  # noqa: E501
-    """
-    Call interface method IVirtualBox::findCloudNetworkByName
-
-    :param select: The object attributes separated by comma
-    :type select: str
-    :param networkName: 
-    :type networkName: str
-
-    :rtype: CloudNetworkResponse
-    """
-
-    return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
+#     return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
 
 
-def i_virtualbox_removecloudnetwork(network=None):  # noqa: E501
-    """
-    Call interface method IVirtualBox::removeCloudNetwork
+# def i_virtualbox_findcloudnetworkbyname(select=None, networkName=None):  # noqa: E501
+#     """
+#     Call interface method IVirtualBox::findCloudNetworkByName
 
-    :param network: Put here an ID of requested ICloudNetwork VirtualBox object
-    :type network: str
+#     :param select: The object attributes separated by comma
+#     :type select: str
+#     :param networkName: 
+#     :type networkName: str
 
-    :rtype: None
-    """
+#     :rtype: CloudNetworkResponse
+#     """
 
-    return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
+#     return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
+
+
+# def i_virtualbox_removecloudnetwork(network=None):  # noqa: E501
+#     """
+#     Call interface method IVirtualBox::removeCloudNetwork
+
+#     :param network: Put here an ID of requested ICloudNetwork VirtualBox object
+#     :type network: str
+
+#     :rtype: None
+#     """
+
+#     return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
