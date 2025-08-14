7d8245937423f6376a493364b03c16dbbf64f02f
Valery Portnyagin <valery.portnyagin@oracle.com>
2025-08-14 16:00:33 +0400

    fix. Commented out some functions that are generated now. Fixed names according new naming convention.

--- vbox_server/controllers/internal/i_natnetwork_controller.py
+++ vbox_server/controllers/internal/i_natnetwork_controller.py
@@ -18,7 +18,7 @@ from vbox_server.models.error import Error  # noqa: E501
 from vbox_server.models.nat_network_add_local_mapping_request_body import NATNetworkAddLocalMappingRequestBody  # noqa: E501
 from vbox_server.models.nat_network_add_port_forward_rule_request_body import NATNetworkAddPortForwardRuleRequestBody  # noqa: E501
 from vbox_server.models.nat_network_remove_port_forward_rule_request_body import NATNetworkRemovePortForwardRuleRequestBody  # noqa: E501
-from vbox_server.models.nat_network_response import NATNetworkResponse  # noqa: E501
+from vbox_server.models.nat_network_obj_wrapper_response import NATNetworkObjWrapperResponse  # noqa: E501
 from vbox_server import util
 
 
@@ -64,68 +64,68 @@ def i_natnetwork_removeportforwardrule(networkid, oNATNetworkRemovePortForwardRu
     return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
 
 
-def i_natnetwork_start(networkid):  # noqa: E501
-    """
-    Call interface method INATNetwork::start
+# def i_natnetwork_start(networkid):  # noqa: E501
+#     """
+#     Call interface method INATNetwork::start
 
-    :param networkid: The Id of network
-    :type networkid: str
+#     :param networkid: The Id of network
+#     :type networkid: str
 
-    :rtype: None
-    """
+#     :rtype: None
+#     """
 
-    return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
-
-
-def i_natnetwork_stop(networkid):  # noqa: E501
-    """
-    Call interface method INATNetwork::stop
-
-    :param networkid: The Id of network
-    :type networkid: str
-
-    :rtype: None
-    """
-
-    return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
+#     return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
 
 
-def i_virtualbox_createnatnetwork(networkName=None):  # noqa: E501
-    """
-    Call interface method IVirtualBox::createNATNetwork
+# def i_natnetwork_stop(networkid):  # noqa: E501
+#     """
+#     Call interface method INATNetwork::stop
 
-    :param networkName: 
-    :type networkName: str
+#     :param networkid: The Id of network
+#     :type networkid: str
 
-    :rtype: NATNetworkResponse
-    """
+#     :rtype: None
+#     """
 
-    return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
-
-
-def i_virtualbox_findnatnetworkbyname(select=None, networkName=None):  # noqa: E501
-    """
-    Call interface method IVirtualBox::findNATNetworkByName
-
-    :param select: The object attributes separated by comma
-    :type select: str
-    :param networkName: 
-    :type networkName: str
-
-    :rtype: NATNetworkResponse
-    """
-
-    return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
+#     return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
 
 
-def i_virtualbox_removenatnetwork(network=None):  # noqa: E501
-    """
-    Call interface method IVirtualBox::removeNATNetwork
+# def i_virtualbox_createnatnetwork(networkName=None):  # noqa: E501
+#     """
+#     Call interface method IVirtualBox::createNATNetwork
 
-    :param network: Put here an ID of requested INATNetwork VirtualBox object
-    :type network: str
+#     :param networkName: 
+#     :type networkName: str
 
-    :rtype: None
-    """
+#     :rtype: NATNetworkResponse
+#     """
 
-    return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
+#     return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
+
+
+# def i_virtualbox_findnatnetworkbyname(select=None, networkName=None):  # noqa: E501
+#     """
+#     Call interface method IVirtualBox::findNATNetworkByName
+
+#     :param select: The object attributes separated by comma
+#     :type select: str
+#     :param networkName: 
+#     :type networkName: str
+
+#     :rtype: NATNetworkResponse
+#     """
+
+#     return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
+
+
+# def i_virtualbox_removenatnetwork(network=None):  # noqa: E501
+#     """
+#     Call interface method IVirtualBox::removeNATNetwork
+
+#     :param network: Put here an ID of requested INATNetwork VirtualBox object
+#     :type network: str
+
+#     :rtype: None
+#     """
+
+#     return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
