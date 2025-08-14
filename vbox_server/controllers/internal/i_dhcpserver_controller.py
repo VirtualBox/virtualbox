0661f875004d06999f770a2e80a33fc22caa531c
Valery Portnyagin <valery.portnyagin@oracle.com>
2025-08-14 15:50:24 +0400

    fix. Commented out some functions that are generated now. Fixed names according new naming convention.

--- vbox_server/controllers/internal/i_dhcpserver_controller.py
+++ vbox_server/controllers/internal/i_dhcpserver_controller.py
@@ -14,12 +14,10 @@ import logging
 from http import HTTPStatus
 from flask import jsonify
 
-from vbox_server.models.dhcp_config_response import DHCPConfigResponse  # noqa: E501
-from vbox_server.models.dhcp_server_response import DHCPServerResponse  # noqa: E501
+from vbox_server.models.dhcp_config_obj_wrapper_response import DHCPConfigObjWrapperResponse  # noqa: E501
+from vbox_server.models.dhcp_server_obj_wrapper_response import DHCPServerObjWrapperResponse  # noqa: E501
 from vbox_server.models.dhcp_server_set_configuration_request_body import DHCPServerSetConfigurationRequestBody  # noqa: E501
 from vbox_server.models.dhcp_server_start_request_body import DHCPServerStartRequestBody  # noqa: E501
-from vbox_server.models.error import Error  # noqa: E501
-from vbox_server import util
 
 
 def i_dhcpserver_getconfig(serverid, select=None, scope=None, name=None, slot=None, mayAdd=None):  # noqa: E501
@@ -45,17 +43,17 @@ def i_dhcpserver_getconfig(serverid, select=None, scope=None, name=None, slot=No
     return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
 
 
-def i_dhcpserver_restart(serverid):  # noqa: E501
-    """
-    Call interface method IDHCPServer::restart
+# def i_dhcpserver_restart(serverid):  # noqa: E501
+#     """
+#     Call interface method IDHCPServer::restart
 
-    :param serverid: The Id of server
-    :type serverid: str
+#     :param serverid: The Id of server
+#     :type serverid: str
 
-    :rtype: None
-    """
+#     :rtype: None
+#     """
 
-    return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
+#     return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
 
 
 def i_dhcpserver_setconfiguration(serverid, oDHCPServerSetConfigurationRequestBody):  # noqa: E501
@@ -88,55 +86,55 @@ def i_dhcpserver_start(serverid, oDHCPServerStartRequestBody):  # noqa: E501
     return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
 
 
-def i_dhcpserver_stop(serverid):  # noqa: E501
-    """
-    Call interface method IDHCPServer::stop
+# def i_dhcpserver_stop(serverid):  # noqa: E501
+#     """
+#     Call interface method IDHCPServer::stop
 
-    :param serverid: The Id of server
-    :type serverid: str
+#     :param serverid: The Id of server
+#     :type serverid: str
 
-    :rtype: None
-    """
+#     :rtype: None
+#     """
 
-    return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
-
-
-def i_virtualbox_createdhcpserver(name=None):  # noqa: E501
-    """
-    Call interface method IVirtualBox::createDHCPServer
-
-    :param name: 
-    :type name: str
-
-    :rtype: DHCPServerResponse
-    """
-
-    return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
+#     return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
 
 
-def i_virtualbox_finddhcpserverbynetworkname(select=None, name=None):  # noqa: E501
-    """
-    Call interface method IVirtualBox::findDHCPServerByNetworkName
+# def i_virtualbox_createdhcpserver(name=None):  # noqa: E501
+#     """
+#     Call interface method IVirtualBox::createDHCPServer
 
-    :param select: The object attributes separated by comma
-    :type select: str
-    :param name: 
-    :type name: str
+#     :param name: 
+#     :type name: str
 
-    :rtype: DHCPServerResponse
-    """
+#     :rtype: DHCPServerResponse
+#     """
 
-    return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
+#     return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
 
 
-def i_virtualbox_removedhcpserver(server=None):  # noqa: E501
-    """
-    Call interface method IVirtualBox::removeDHCPServer
+# def i_virtualbox_finddhcpserverbynetworkname(select=None, name=None):  # noqa: E501
+#     """
+#     Call interface method IVirtualBox::findDHCPServerByNetworkName
 
-    :param server: Put here an ID of requested IDHCPServer VirtualBox object
-    :type server: str
+#     :param select: The object attributes separated by comma
+#     :type select: str
+#     :param name: 
+#     :type name: str
 
-    :rtype: None
-    """
+#     :rtype: DHCPServerResponse
+#     """
 
-    return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
+#     return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
+
+
+# def i_virtualbox_removedhcpserver(server=None):  # noqa: E501
+#     """
+#     Call interface method IVirtualBox::removeDHCPServer
+
+#     :param server: Put here an ID of requested IDHCPServer VirtualBox object
+#     :type server: str
+
+#     :rtype: None
+#     """
+
+#     return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
