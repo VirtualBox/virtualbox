34e0c9de49b6bd472876749cecc4c3b528fb4fad
Valery Portnyagin <valery.portnyagin@oracle.com>
2025-09-24 11:27:27 +0400

    feat. Deleted accidentally added duplication of mediumDecorator implementation.

--- vbox_server/utils/decorators.py
+++ vbox_server/utils/decorators.py
@@ -683,64 +683,6 @@ def natnetworkDecorator(func):
     return wrapper_decorator
 
 
-def __find_medium_by_id(id: str):
-    lDiskType = ['hardDisks', 'DVDImages', 'floppyImages']
-    oVBox = ctx['vb']
-    fFound = False
-    oFoundMedium = None
-    oError = None
-
-    for diskType in lDiskType:
-        try:
-            olDisks = ctx['global'].getArray(oVBox, diskType)
-            for item in olDisks:
-                if str(item.id) == id:
-                    oFoundMedium = item
-                    fFound = True
-                    break
-        except Exception as e:
-            logging.info('Error walking through the array of ' + diskType)
-            httpCode = HTTPStatus.INTERNAL_SERVER_ERROR
-            oError = Error(httpCode, str(e))
-            oFoundMedium = None
-
-        if fFound is True:
-            break
-
-    return oFoundMedium, oError
-
-
-def mediumDecorator(func):
-    """
-    Find the medium object using the passed ID
-    The first parameter must be medium uuid always.
-    Appends the arguments list by the flag which indicates whether the medium was found or not
-    """
-    @functools.wraps(func)
-    def wrapper_decorator(*args, **kwargs):
-        args_repr = [a for a in args]
-        mediumid = args_repr[0]
-        oVBoxMedium = None
-
-        oVBoxMedium, oError = __find_medium_by_id(mediumid)
-        if oVBoxMedium is not None:
-            args_repr[0] = oVBoxMedium #replace the first argument "mediumid" by oVBoxMedium
-        else:
-            if oError:
-                return jsonify('The medium with UUID ' + mediumid + ' wasn\'t found. Internal error is ' + '"' + oError.message + '"'), oError.code
-            else:
-                return jsonify("The medium with UUID " + mediumid + " wasn't found"), HTTPStatus.NOT_FOUND
-
-        new_args_repr=args_repr
-
-        #Call the general function with the updated arguments list
-        value = func(*new_args_repr, **kwargs)
-
-        return value
-
-    return wrapper_decorator
-
-
 dhcpgroupconfigDecorator = commonObjDecorator
 dhcpconfigDecorator = commonObjDecorator
 dhcpgroupconditionDecorator = commonObjDecorator
