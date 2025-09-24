5f5796bbdd505574ca446a4f07b65e9f77185033
Valery Portnyagin <valery.portnyagin@oracle.com>
2025-09-24 13:14:57 +0400

    feat. Added i_virtualbox_createsharedfolder implementation.

--- vbox_server/controllers/internal/i_sharedfolder_controller.py
+++ vbox_server/controllers/internal/i_sharedfolder_controller.py
@@ -14,12 +14,14 @@ import logging
 from http import HTTPStatus
 from flask import jsonify
 
+from vbox_server.utils.decorators import *
 from vbox_server.models.error import Error  # noqa: E501
 from vbox_server.models.virtual_box_create_shared_folder_request_body import VirtualBoxCreateSharedFolderRequestBody  # noqa: E501
 from vbox_server import util
 
 
-def i_virtualbox_createsharedfolder(oVirtualBoxCreateSharedFolderRequestBody):  # noqa: E501
+@virtualboxDecorator
+def i_virtualbox_createsharedfolder(oVBoxObj, oVirtualBoxCreateSharedFolderRequestBody):  # noqa: E501
     """
     Call interface method IVirtualBox::createSharedFolder
 
@@ -28,7 +30,38 @@ def i_virtualbox_createsharedfolder(oVirtualBoxCreateSharedFolderRequestBody):  
 
     :rtype: None
     """
-    return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
+
+    vbox_utils_commonChecks()
+    httpCode = HTTPStatus.OK
+    oError = None
+
+    oCurrVirtualBox = oVBoxObj
+    
+    o = oVirtualBoxCreateSharedFolderRequestBody
+    name = o.name
+    hostPath = o.hostPath
+    fWritable = o.writable
+    fAutomount = o.automount
+    autoMountPoint = o.autoMountPoint
+
+    logging.info(f'Try to create the shared folder {name}')
+
+    oError = None
+    httpCode = HTTPStatus.OK
+
+    if oError is None:
+        try:
+            # No return result check
+            oCurrVirtualBox.createSharedFolder(name, hostPath, fWritable, fAutomount, autoMountPoint)
+            logging.info(f'Created the shared folder {name}')
+
+        except Exception as e:
+            logging.info(f'Exception during creation the shared folder {name}')
+            httpCode = HTTPStatus.INTERNAL_SERVER_ERROR
+            oError = Error(httpCode, str(e))
+
+    response = jsonify(oError if oError is not None else f'Successfully created the shared folder {name}')
+    return response, httpCode
 
 
 # def i_virtualbox_removesharedfolder(name=None):  # noqa: E501
