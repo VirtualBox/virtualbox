b15c27ad0ec8b0eef282c8d3584134d068924fa7
Valery Portnyagin <valery.portnyagin@oracle.com>
2026-02-03 18:45:19 +0400

    fix. Preparation for the transition to generated functions. Phase 1. Functions that are now generated have been commented out. Some functions have been moved to other files. Testing is required.

--- vbox_server/controllers/internal/i_progress_controller.py
+++ vbox_server/controllers/internal/i_progress_controller.py
@@ -20,55 +20,42 @@ from vbox_server.utils.vbox_utils import *
 from vbox_server.utils.object_conversion import *
 
 from vbox_server.models.error import Error  # noqa: E501
-from vbox_server.models.progress_obj_wrapper_response import ProgressObjWrapperResponse  # noqa: E501
+# from vbox_server.models.progress_obj_wrapper_response import ProgressObjWrapperResponse  # noqa: E501
 
 
-def i_progress_cancel(progressid):  # noqa: E501
-    """
-    Call interface method IProgress::cancel
+# def i_virtualbox_findprogressbyid(progressid, select=None, id=None):  # noqa: E501
+#     """
+#     Call interface method IVirtualBox::findProgressById
 
-    :param progressid: The Id of progress
-    :type progressid: str
+#     :param progressid: The Id of progress
+#     :type progressid: str
+#     :param select: The object attributes separated by comma
+#     :type select: str
+#     :param id: 
+#     :type id: str
 
-    :rtype: None
-    """
+#     :rtype: ProgressResponse
+#     """
+#     oError = None
+#     httpCode = 200 #(OK)
 
-    return "Not implemented yet", HTTPStatus.NOT_IMPLEMENTED
+#     vbox_utils_commonChecks()
 
+#     try:
+#         oVBox = ctx['vb']
+#     except Exception as e:
+#         logging.info ('couldn\'t get the VirtualBox object')
+#         oError = Error(500, str(e))
+#         return jsonify(oError), 500
 
-def i_virtualbox_findprogressbyid(progressid, select=None, id=None):  # noqa: E501
-    """
-    Call interface method IVirtualBox::findProgressById
+#     oProgressResponse = ProgressObjWrapperResponse()
+#     try:
+#         oVBoxProgress = oVBox.findProgressById(progressid)
+#         oProgressResponse.progress = i_fill_progress(oVBoxProgress)
+#     except Exception as e:
+#         httpCode = 500
+#         oError = Error(httpCode, str(e))
 
-    :param progressid: The Id of progress
-    :type progressid: str
-    :param select: The object attributes separated by comma
-    :type select: str
-    :param id: 
-    :type id: str
-
-    :rtype: ProgressResponse
-    """
-    oError = None
-    httpCode = 200 #(OK)
-
-    vbox_utils_commonChecks()
-
-    try:
-        oVBox = ctx['vb']
-    except Exception as e:
-        logging.info ('couldn\'t get the VirtualBox object')
-        oError = Error(500, str(e))
-        return jsonify(oError), 500
-
-    oProgressResponse = ProgressObjWrapperResponse()
-    try:
-        oVBoxProgress = oVBox.findProgressById(progressid)
-        oProgressResponse.progress = i_fill_progress(oVBoxProgress)
-    except Exception as e:
-        httpCode = 500
-        oError = Error(httpCode, str(e))
-
-    response = jsonify(oError if oError is not None else oProgressResponse)
-    return response, httpCode
+#     response = jsonify(oError if oError is not None else oProgressResponse)
+#     return response, httpCode
 
