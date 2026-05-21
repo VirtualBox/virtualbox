eb6f817c463c106e0161b0c0e39337dc06364551
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2026-05-21 14:32:33 +0000

    7.2: Backported r173980: SUPDrv/linux: Corrected wrong kernel_fpu_end/fpregs_unlock order in SUPR0FpuEnd (no ctx/kvm branch). bugref:10891 oem2ticketref:115 oem2ticketref:132
    svn:sync-xref-src-repo-rev: r173987

--- src/VBox/HostDrivers/Support/linux/SUPDrv-linux.c
+++ src/VBox/HostDrivers/Support/linux/SUPDrv-linux.c
@@ -1,4 +1,4 @@
-/* $Id: SUPDrv-linux.c 114150 2026-05-19 11:56:11Z knut.osmundsen@oracle.com $ */
+/* $Id: SUPDrv-linux.c 114172 2026-05-21 14:32:33Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBoxDrv - The VirtualBox Support Driver - Linux specifics.
  */
@@ -2101,6 +2101,7 @@ SUPR0DECL(uint32_t) SUPR0FpuBegin(bool fCtxHook)
         fpregs_lock();
         kernel_fpu_begin();
         /** @todo consider setting SUPR0FPU_BEGIN_F_FULLY_SAVED in fBegin */
+        /** @todo not entirely logical not to set SUPR0FPU_BEGIN_F_HOST_STATE_LOCKED here... */
     }
     local_irq_restore(fSavedIrqs);
     Assert(fCtxHook || !irq_fpu_usable());
@@ -2209,8 +2210,8 @@ SUPR0DECL(void) SUPR0FpuEnd(uint32_t fBegin)
         /* Always disable IRQs and explicitly unlock the FPU state. */
         unsigned long fSavedIrqs;
         local_irq_save(fSavedIrqs);
-        fpregs_unlock();
         kernel_fpu_end();
+        fpregs_unlock();
         local_irq_restore(fSavedIrqs);
     }
 
