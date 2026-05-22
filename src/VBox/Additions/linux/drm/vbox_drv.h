443c3e8a707ad990a50db98424a2c057f82c1671
Vadim Galitsyn <vadim.galitsyn@oracle.com>
2026-05-22 11:34:30 +0000

    7.1: Backported r173989, r173990: Linux: iprt, Additions: Add initial support for RHEL 9.9 kernel (host+guest), bugref:10908, bugref:10991.
    svn:sync-xref-src-repo-rev: r173991

--- src/VBox/Additions/linux/drm/vbox_drv.h
+++ src/VBox/Additions/linux/drm/vbox_drv.h
@@ -1,4 +1,4 @@
-/* $Id: vbox_drv.h 113477 2026-03-19 16:14:34Z vadim.galitsyn@oracle.com $ */
+/* $Id: vbox_drv.h 114176 2026-05-22 11:34:30Z vadim.galitsyn@oracle.com $ */
 /** @file
  * VirtualBox Additions Linux kernel video driver
  */
@@ -306,7 +306,7 @@ struct vbox_private {
 	} ttm;
 
 	struct mutex hw_mutex; /* protects modeset and accel/vbva accesses */
-#if RTLNX_VER_MIN(6,18,0) || RTLNX_RHEL_RANGE(10,2, 10,99)
+#if RTLNX_VER_MIN(6,18,0) || RTLNX_RHEL_RANGE(9,9, 9,99) || RTLNX_RHEL_RANGE(10,2, 10,99)
 	/** Replacement for struct drm_device .struct_mutex. */
 	struct mutex struct_mutex;
 #endif
