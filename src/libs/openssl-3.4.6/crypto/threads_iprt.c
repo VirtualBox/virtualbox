3f2fb59985ede584555bd7c0d34139fbd60e508a
Aleksey Ilyushin <aleksey.ilyushin@oracle.com>
2026-06-12 06:37:33 +0000

    7.2: Backported r174164: openssl-3.4.6: Use RTNATIVETHREAD instead of RTTHREAD for CRYPTO_THREAD_ID as RTThreadSelf() will return NIL for alien threads, which is problematic for windows thread pool threads (COM). ​bugref:11103
    svn:sync-xref-src-repo-rev: r174166

--- src/libs/openssl-3.4.6/crypto/threads_iprt.c
+++ src/libs/openssl-3.4.6/crypto/threads_iprt.c
@@ -1,4 +1,4 @@
-/* $Id: threads_iprt.c 114333 2026-06-10 15:01:11Z alexander.rudnev@oracle.com $ */
+/* $Id: threads_iprt.c 114343 2026-06-12 06:37:33Z aleksey.ilyushin@oracle.com $ */
 /** @file
  * Crypto threading and atomic functions built upon IPRT.
  */
@@ -300,7 +300,7 @@ int CRYPTO_THREAD_cleanup_local(CRYPTO_THREAD_LOCAL *key)
 
 CRYPTO_THREAD_ID CRYPTO_THREAD_get_current_id(void)
 {
-    return RTThreadSelf();
+    return RTThreadNativeSelf();
 }
 
 int CRYPTO_THREAD_compare_id(CRYPTO_THREAD_ID a, CRYPTO_THREAD_ID b)
