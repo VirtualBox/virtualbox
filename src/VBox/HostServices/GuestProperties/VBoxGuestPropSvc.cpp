53a4f5b11e6890f27b0a67f591480a7ab4866b49
Andreas Loeffler <andreas.loeffler@oracle.com>
2026-07-08 09:05:01 +0000

    7.1: Backported r174482: HostServices/GuestProperties: Don't try to memcpy NULL / 0 bytes when deleting guest properties (pszValue == NULL).
    svn:sync-xref-src-repo-rev: r174484

--- src/VBox/HostServices/GuestProperties/VBoxGuestPropSvc.cpp
+++ src/VBox/HostServices/GuestProperties/VBoxGuestPropSvc.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxGuestPropSvc.cpp 106320 2024-10-15 12:08:41Z klaus.espenlaub@oracle.com $ */
+/* $Id: VBoxGuestPropSvc.cpp 114649 2026-07-08 09:05:01Z andreas.loeffler@oracle.com $ */
 /** @file
  * Guest Property Service: Host service entry points.
  */
@@ -1363,20 +1363,23 @@ int Service::notifyHost(const char *pszName, const char *pszValue, uint64_t nsTi
         pHostCallbackData->u32Magic     = GUESTPROPHOSTCALLBACKDATA_MAGIC;
 
         pHostCallbackData->pcszName     = (const char *)pu8;
-        memcpy(pu8, pszName, cbName);
+        if (cbName)
+            memcpy(pu8, pszName, cbName);
         pu8 += cbName;
         *pu8++ = 0;
 
         /* NULL value means property was deleted. */
         pHostCallbackData->pcszValue    = pszValue ? (const char *)pu8 : NULL;
-        memcpy(pu8, pszValue, cbValue);
+        if (cbValue)
+            memcpy(pu8, pszValue, cbValue);
         pu8 += cbValue;
         *pu8++ = 0;
 
         pHostCallbackData->u64Timestamp = nsTimestamp;
 
         pHostCallbackData->pcszFlags    = (const char *)pu8;
-        memcpy(pu8, pszFlags, cbFlags);
+        if (cbFlags)
+            memcpy(pu8, pszFlags, cbFlags);
         pu8 += cbFlags;
         *pu8++ = 0;
 
