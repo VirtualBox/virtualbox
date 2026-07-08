18c0308ddaec77bfdffef1cd9dbc9318a1968da7
Vadim Galitsyn <vadim.galitsyn@oracle.com>
2026-07-08 14:12:45 +0000

    7.1: Backported r174497: Additions: Linux: vboxvideo: Extra fixes for RHEL 9.8 kernel, bugref:11038.
    svn:sync-xref-src-repo-rev: r174498

--- src/VBox/Additions/linux/drm/vbox_ttm.c
+++ src/VBox/Additions/linux/drm/vbox_ttm.c
@@ -1,4 +1,4 @@
-/* $Id: vbox_ttm.c 114655 2026-07-08 10:34:51Z vadim.galitsyn@oracle.com $ */
+/* $Id: vbox_ttm.c 114663 2026-07-08 14:12:45Z vadim.galitsyn@oracle.com $ */
 /** @file
  * VirtualBox Additions Linux kernel video driver
  */
@@ -454,7 +454,7 @@ int vbox_mm_init(struct vbox_private *vbox)
 #elif RTLNX_VER_MAX(5,2,0) && !RTLNX_RHEL_MAJ_PREREQ(8,2)
 				 DRM_FILE_PAGE_OFFSET,
 #endif
-#if RTLNX_VER_RANGE(5,11,0, 6,19,0) && !RTLNX_RHEL_RANGE(8,5, 9,99)
+#if (RTLNX_VER_RANGE(5,11,0, 6,19,0) && !RTLNX_RHEL_RANGE(9,9, 9,99)) || RTLNX_RHEL_RANGE(8,5, 8,99) || RTLNX_RHEL_RANGE(9,8, 9,9)
 				 false,
 #endif
 #if RTLNX_VER_MIN(6,19,0) || RTLNX_RHEL_RANGE(9,9, 9,99)
