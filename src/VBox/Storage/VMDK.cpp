e4ea53070f05bb9f55cb885e14f6dc6cd39a5599
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2026-07-14 10:18:05 +0000

    7.2: Backported r174524: Storage/VMDK: Fix typo. bugref:11122
    svn:sync-xref-src-repo-rev: r174525

--- src/VBox/Storage/VMDK.cpp
+++ src/VBox/Storage/VMDK.cpp
@@ -1,4 +1,4 @@
-/* $Id: VMDK.cpp 114686 2026-07-14 10:12:04Z klaus.espenlaub@oracle.com $ */
+/* $Id: VMDK.cpp 114689 2026-07-14 10:18:05Z klaus.espenlaub@oracle.com $ */
 /** @file
  * VMDK disk image, core code.
  */
@@ -854,7 +854,7 @@ DECLINLINE(int) vmdkFileInflateSync(PVMDKIMAGE pImage, PVMDKEXTENT pExtent,
     /* Sanity check - the expansion ratio should be much less than 2. */
     Assert(cbCompSize < 2 * cbToRead);
     if (   cbCompSize >= 2 * cbToRead
-        || RT_ALIGN_Z(cbCompSize + RT_UOFFSETOF(VMDKMARKER, uType), 512) > cbCompGrain)
+        || RT_ALIGN_Z(cbCompSize + RT_UOFFSETOF(VMDKMARKER, uType), 512) > pExtent->cbCompGrain)
         return VERR_VD_VMDK_INVALID_FORMAT;
 
     /* Compressed grain marker. Data follows immediately. */
