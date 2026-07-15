e69bf8ada3e63cd8fcd1fe1aaded41d7f078cb4c
Vitali Pelenjow <vitali.pelenjow@oracle.com>
2026-07-15 17:29:10 +0000

    7.1: backported r174548: Devices/Graphics: COTable grow: use size of old COTable. bugref:11124
    svn:sync-xref-src-repo-rev: r174550

--- src/VBox/Devices/Graphics/DevVGA-SVGA3d-dx.cpp
+++ src/VBox/Devices/Graphics/DevVGA-SVGA3d-dx.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevVGA-SVGA3d-dx.cpp 114299 2026-06-09 14:16:55Z vitali.pelenjow@oracle.com $ */
+/* $Id: DevVGA-SVGA3d-dx.cpp 114714 2026-07-15 17:29:10Z vitali.pelenjow@oracle.com $ */
 /** @file
  * DevSVGA3d - VMWare SVGA device, 3D parts - Common code for DX backend interface.
  */
@@ -2438,11 +2438,13 @@ static int dxSetOrGrowCOTable(PVGASTATECC pThisCC, PVMSVGA3DDXCONTEXT pDXContext
 
     /* Set the new host buffer and remember the old host buffer. */
     void *pvOldCOT = NULL;
+    uint32_t cbOldCOT = 0;
     switch (enmType)
     {
 #   define CASE_DX_COT(_COTName, _COTEntryType, _COTField) \
         case _COTName: \
             pvOldCOT                        = pDXContext->cot.pa ## _COTField; \
+            cbOldCOT                        = pDXContext->cot.c ## _COTField * sizeof(_COTEntryType); \
             pDXContext->cot.pa ## _COTField = (_COTEntryType *)pvCOT; \
             pDXContext->cot.c ## _COTField  = cEntries; \
             break;
@@ -2480,7 +2482,7 @@ static int dxSetOrGrowCOTable(PVGASTATECC pThisCC, PVMSVGA3DDXCONTEXT pDXContext
              */
             Assert(pvOldCOT && pvCOT);
             if (pvOldCOT)
-                memcpy(pvCOT, pvOldCOT, validSizeInBytes);
+                memcpy(pvCOT, pvOldCOT, RT_MIN(validSizeInBytes, cbOldCOT));
         }
         else
         {
