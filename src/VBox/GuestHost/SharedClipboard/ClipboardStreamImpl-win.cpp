483eda03ac9b05b45aeed8aceca32e56928ebf50
Andreas Loeffler <andreas.loeffler@oracle.com>
2026-08-18 14:45:28 +0000

    Shared Clipboard/Win: Handle different transfer chunk sizes more gracefully. Added a new testcase for this.
    svn:sync-xref-src-repo-rev: r174909

--- src/VBox/GuestHost/SharedClipboard/ClipboardStreamImpl-win.cpp
+++ src/VBox/GuestHost/SharedClipboard/ClipboardStreamImpl-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: ClipboardStreamImpl-win.cpp 115050 2026-08-17 15:20:35Z andreas.loeffler@oracle.com $ */
+/* $Id: ClipboardStreamImpl-win.cpp 115068 2026-08-18 14:45:28Z andreas.loeffler@oracle.com $ */
 /** @file
  * ClipboardStreamImpl-win.cpp - Shared Clipboard IStream object implementation (guest and host side).
  */
@@ -257,11 +257,25 @@ STDMETHODIMP ShClWinStreamImpl::Read(void *pvBuffer, ULONG nBytesToRead, ULONG *
     {
         if (cbToRead)
         {
-            rc = ShClTransferObjRead(m_pTransfer, m_hObj, pvBuffer, cbToRead, 0 /* fFlags */, &cbRead);
-            if (RT_SUCCESS(rc))
+            /* Windows treats a short IStream read as EOF, so satisfy it using
+             * as many transfer chunks as necessary. */
+            while (cbRead < cbToRead)
             {
-                m_cbProcessed += cbRead;
+                uint32_t const cbToReadChunk = RT_MIN(cbToRead - cbRead, m_pTransfer->cbMaxChunkSize);
+                uint32_t       cbReadChunk   = 0;
+                rc = ShClTransferObjRead(m_pTransfer, m_hObj, (uint8_t *)pvBuffer + cbRead, cbToReadChunk,
+                                         0 /* fFlags */, &cbReadChunk);
+                if (RT_FAILURE(rc))
+                    break;
+
+                AssertBreakStmt(cbReadChunk <= cbToReadChunk, rc = VERR_TOO_MUCH_DATA);
+
+                cbRead        += cbReadChunk;
+                m_cbProcessed += cbReadChunk;
                 Assert(m_cbProcessed <= cbSize);
+
+                if (cbReadChunk < cbToReadChunk)
+                    break;
             }
         }
 
