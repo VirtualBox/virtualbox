756acc4357639cacd5038f73bdf791b3b595c650
Vadim Galitsyn <vadim.galitsyn@oracle.com>
2026-09-01 10:17:38 +0000

    Additions: vboxvideo: Fix build for kernel 6.12.103, bugref:10782, bugref:11038, github:gh-845.
    svn:sync-xref-src-repo-rev: r174987

--- src/VBox/Additions/linux/drm/vbox_fb.c
+++ src/VBox/Additions/linux/drm/vbox_fb.c
@@ -1,4 +1,4 @@
-/* $Id: vbox_fb.c 114629 2026-07-06 15:59:47Z vadim.galitsyn@oracle.com $ */
+/* $Id: vbox_fb.c 115141 2026-09-01 10:17:38Z vadim.galitsyn@oracle.com $ */
 /** @file
  * VirtualBox Additions Linux kernel video driver
  */
@@ -335,7 +335,7 @@ int vboxfb_create(struct drm_fb_helper *helper,
 		return ret;
 	}
 
-#if RTLNX_VER_MIN(6,19,0) || RTLNX_RHEL_RANGE(9,9, 9,99)
+#if RTLNX_VER_MIN(6,19,0) || RTLNX_VER_RANGE(6,12,103, 6,13,0) || RTLNX_RHEL_RANGE(9,9, 9,99)
 	info = helper->info;
 #elif RTLNX_VER_MIN(6,2,0) || RTLNX_RHEL_RANGE(8,9, 8,99) || RTLNX_RHEL_RANGE(9,3, 9,99)
 	info = drm_fb_helper_alloc_info(helper);
