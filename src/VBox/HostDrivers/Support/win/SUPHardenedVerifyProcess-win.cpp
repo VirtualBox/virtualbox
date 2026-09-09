f7def98e8ff1c701debede0e2a2fea77d441406c
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2026-09-09 11:56:54 +0000

    7.2: Backported r175057: SUPHardNt: Relax attitude to rwx memory in the initial process verfication. (testboxmem1 failures) bugref:11172
    svn:sync-xref-src-repo-rev: r175058

--- src/VBox/HostDrivers/Support/win/SUPHardenedVerifyProcess-win.cpp
+++ src/VBox/HostDrivers/Support/win/SUPHardenedVerifyProcess-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: SUPHardenedVerifyProcess-win.cpp 113916 2026-04-16 21:00:28Z knut.osmundsen@oracle.com $ */
+/* $Id: SUPHardenedVerifyProcess-win.cpp 115210 2026-09-09 11:56:54Z knut.osmundsen@oracle.com $ */
 /** @file
  * VirtualBox Support Library/Driver - Hardened Process Verification, Windows.
  */
@@ -2107,7 +2107,8 @@ static int supHardNtVpScanVirtualMemory(PSUPHNTVPSTATE pThis, HANDLE hProcess)
                                         MemInfo.Type, MemInfo.AllocationBase, MemInfo.BaseAddress, MemInfo.RegionSize);
                 pThis->cFixes++;
             }
-            else if (pThis->enmKind != SUPHARDNTVPKIND_SELF_PURIFICATION_LIMITED)
+            else if (   pThis->enmKind != SUPHARDNTVPKIND_SELF_PURIFICATION_LIMITED
+                     && pThis->enmKind != SUPHARDNTVPKIND_LIMITED_VERIFY_ONLY)
 # endif /* IN_RING3 */
                 supHardNtVpSetInfo2(pThis, VERR_SUP_VP_FOUND_EXEC_MEMORY,
                                     "Found executable memory at %p (%p LB %#zx): type=%#x prot=%#x state=%#x aprot=%#x abase=%p",
