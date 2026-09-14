bdd021a39584b1a937282ff962e5c0eff87137f3
Aleksey Ilyushin <aleksey.ilyushin@oracle.com>
2026-09-14 07:46:03 +0000

    7.2: Backported r175104: Windows build fix for r175102 bugref:11154
    svn:sync-xref-src-repo-rev: r175105

--- src/VBox/Main/src-global/win/VBoxSDS.cpp
+++ src/VBox/Main/src-global/win/VBoxSDS.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxSDS.cpp 115252 2026-09-14 07:31:59Z aleksey.ilyushin@oracle.com $ */
+/* $Id: VBoxSDS.cpp 115254 2026-09-14 07:46:03Z aleksey.ilyushin@oracle.com $ */
 /** @file
  * VBoxSDS - COM global service main entry (System Directory Service)
  */
@@ -1034,7 +1034,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
      * Initialize the hardened image verification (nop in non-hardened builds).
      * We need this later for verfiying alleged VBoxSVC processes.
      */
-    int vrc = SUPR3HardenedVerifyInit();
+    vrc = SUPR3HardenedVerifyInit();
     if (RT_FAILURE(vrc))
         LogRel(("ERROR: SUPR3HardenedVerifyInit failed: %Rrc\n", vrc));
 
