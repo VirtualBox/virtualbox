d0d101e4630e775f377369e9f63e8e45b270c3fc
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2026-09-21 09:48:06 +0000

    tstRTCritSect: adjust fudge for tstRTCritSectW32.
    svn:sync-xref-src-repo-rev: r175144

--- src/VBox/Runtime/testcase/tstRTCritSect.cpp
+++ src/VBox/Runtime/testcase/tstRTCritSect.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstRTCritSect.cpp 112403 2026-01-11 19:29:08Z knut.osmundsen@oracle.com $ */
+/* $Id: tstRTCritSect.cpp 115292 2026-09-21 09:48:06Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT Testcase - Critical Sections.
  */
@@ -232,7 +232,11 @@ static int Test1(unsigned cThreads)
         /* wait for it to get into waiting. */
         while (LOCKERS(CritSect) == iLock)
             RTThreadSleep(10);
+#ifdef TRY_WIN32_CRIT
+        RTThreadSleep(42); /* increased fudge to try combat sporadic failurs... */
+#else
         RTThreadSleep(20);
+#endif
     }
 
     /*
@@ -504,7 +508,7 @@ int main(int argc, char **argv)
                 return 1;
 
             case 'V':
-                RTPrintf("$Revision: 112403 $\n");
+                RTPrintf("$Revision: 115292 $\n");
                 return 0;
 
             default:
