cf63f083c592fdc26748eaef334afab7d9f147f5
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2026-09-21 18:14:07 +0000

    ValKit/tdUnitTest1.py: Added an exclusion list based on X11 dependency and use ctypes.util.find_library to check if libX11.so* is there. Should fix the tstClipboardMockHGCM failures on some 7.2 test (not all testboxes has X11).  Some cleaning up: multiline if  expressions shall be put inside () so we don't have to escape newlines and to make it easier to read.
    svn:sync-xref-src-repo-rev: r175147

--- src/VBox/ValidationKit/tests/unittests/tdUnitTest1.py
+++ src/VBox/ValidationKit/tests/unittests/tdUnitTest1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: tdUnitTest1.py 115050 2026-08-17 15:20:35Z andreas.loeffler@oracle.com $
+# $Id: tdUnitTest1.py 115295 2026-09-21 18:14:07Z knut.osmundsen@oracle.com $
 
 """
 VirtualBox Validation Kit - Unit Tests.
@@ -37,13 +37,14 @@ terms and conditions of either the GPL or the CDDL or both.
 
 SPDX-License-Identifier: GPL-3.0-only OR CDDL-1.0
 """
-__version__ = "$Revision: 115050 $"
+__version__ = "$Revision: 115295 $"
 
 
 # Standard Python imports.
 import os
 import sys
 import re
+from ctypes import util as ct_util;
 
 
 # Only the main script needs to modify the path.
@@ -264,7 +265,12 @@ class tdUnitTest1(vbox.TestDriver):
         'testcase/tstVMMR0CallHost-1': '',              # Triggers a stack overflow error on linux.amd64
         'testcase/tstRTProcCreateEx' : '',              # Triggers on some linux hosts where a PAM module probably
                                                         # introduces a memory leak.
-    }
+    };
+
+    ## List of testcases that requires X11.
+    kdTestCasesNeedingX11 = {
+        'testcase/tstClipboardMockHGCM': '',            # 7.2 and earlier.
+    };
 
     # Suffix exclude list.
     kasSuffixBlackList = [
@@ -1229,11 +1235,15 @@ class tdUnitTest1(vbox.TestDriver):
             oDevNull = None;
 
         # Determin the host OS specific exclusion lists.
-        dTestCasesBuggyForHostOs = self.kdTestCasesBuggyPerOs.get(utils.getHostOs(), []);
+        sHostOs = utils.getHostOs();
+        dTestCasesBuggyForHostOs = self.kdTestCasesBuggyPerOs.get(sHostOs, []);
         dTestCasesBuggyForHostOs.update(self.kdTestCasesBuggyPerOs.get(utils.getHostOsDotArch(), []));
 
-        ## @todo Add filtering for more specific OSes (like OL server, doesn't have X installed) by adding a separate
-        #        black list + using utils.getHostOsVersion().
+        # Exclude because of missing library.
+        dTestCaseMissingDep = {};
+        if not self.isRemoteMode() and sHostOs not in ('freebsd', 'os2', 'win',):
+            if not ct_util.find_library('X11'):
+                dTestCaseMissingDep.update(self.kdTestCasesNeedingX11);
 
         #
         # Process the file list and run everything looking like a testcase.
@@ -1268,15 +1278,15 @@ class tdUnitTest1(vbox.TestDriver):
                           % (sTestCasePattern, sBaseName, sName, sSuffix, sFilename,));
 
             # Process white list first, if set.
-            if  self.fOnlyWhiteList \
-            and not self._isExcluded(sName, self.kdTestCasesWhiteList):
+            if (    self.fOnlyWhiteList
+                and not self._isExcluded(sName, self.kdTestCasesWhiteList)):
                 # (No testStart/Done or accounting here!)
                 reporter.log('%s: SKIPPED (not in white list)' % (sName,));
                 continue;
 
             # Basic exclusion.
-            if  not re.match(sTestCasePattern, sBaseName) \
-            or  sSuffix in self.kasSuffixBlackList:
+            if (   not re.match(sTestCasePattern, sBaseName)
+                or sSuffix in self.kasSuffixBlackList):
                 reporter.log2('"%s" is not a test case.' % (sName,));
                 continue;
 
@@ -1296,18 +1306,26 @@ class tdUnitTest1(vbox.TestDriver):
                     continue;
 
                 # Some testcases don't work with ASAN.
-                if self.getBuildType() == 'asan' \
-                and self._isExcluded(sName, self.kdTestCasesBlackListAsan):
+                if (    self.getBuildType() == 'asan'
+                    and self._isExcluded(sName, self.kdTestCasesBlackListAsan)):
                     # (No testStart/Done or accounting here!)
                     reporter.log('%s: SKIPPED (blacklisted ASAN)' % (sName,));
                     continue;
 
                 if self._isExcluded(sName, dTestCasesBuggyForHostOs):
                     reporter.testStart(sName);
-                    reporter.log('%s: Skipping, buggy on %s.' % (sName, utils.getHostOs(),));
+                    reporter.log('%s: Skipping, buggy on %s.' % (sName, sHostOs,));
                     reporter.testDone(fSkipped = True);
                     self.cSkipped += 1;
                     continue;
+
+                if self._isExcluded(sName, dTestCaseMissingDep):
+                    reporter.testStart(sName);
+                    reporter.log('%s: Skipping, missing dependency.' % (sName,));
+                    reporter.testDone(fSkipped = True);
+                    self.cSkipped += 1;
+                    continue;
+
             else:
                 # Passed the white list check already above.
                 pass;
