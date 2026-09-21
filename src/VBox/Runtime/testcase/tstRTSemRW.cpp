d1b8808ef970e79b841e6fdfdca8576f72ec1afd
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2026-09-21 09:29:30 +0000

    tstRTSemRW: Increased the RTThreadWait timeout in Test4.
    svn:sync-xref-src-repo-rev: r175141

--- src/VBox/Runtime/testcase/tstRTSemRW.cpp
+++ src/VBox/Runtime/testcase/tstRTSemRW.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstRTSemRW.cpp 112403 2026-01-11 19:29:08Z knut.osmundsen@oracle.com $ */
+/* $Id: tstRTSemRW.cpp 115289 2026-09-21 09:29:30Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT Testcase - Reader/Writer Semaphore.
  */
@@ -273,7 +273,7 @@ static void Test4(unsigned cThreads, unsigned cSeconds, unsigned uWritePercent, 
      * Clean up the threads and semaphore.
      */
     for (i = 0; i < cThreads; i++)
-        RTTEST_CHECK_RC(g_hTest, RTThreadWait(aThreads[i], 5000, NULL), VINF_SUCCESS);
+        RTTEST_CHECK_RC(g_hTest, RTThreadWait(aThreads[i], 20000, NULL), VINF_SUCCESS);
 
     RTTEST_CHECK_MSG(g_hTest, g_cConcurrentWriters == 0, (g_hTest, "g_cConcurrentWriters=%u at end of test\n", g_cConcurrentWriters));
     RTTEST_CHECK_MSG(g_hTest, g_cConcurrentReaders == 0, (g_hTest, "g_cConcurrentReaders=%u at end of test\n", g_cConcurrentReaders));
